398353f785871466d9a46f5cceef07763720ba92
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-03-22 23:32:43 +0100

    Added password reset

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +19,71 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*------------------------
+    FRONTOFFICE ROUTES
+------------------------*/
 Route::get('/', function () {
     return view('welcome');
 });
 
+/*------------------------
+    AUTH ROUTES
+------------------------*/
 Auth::routes();
 
+/*------------------------
+    EMAIL VERIFICATION
+------------------------*/
+// Modulo di richiesta di collegamento per la reimpostazione della password
+Route::get('/forgot-password', function () {
+    return view('auth.passwords.email');
+})->middleware('guest')->name('password.request');
+
+// Gestione invio modulo
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+ 
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+ 
+    return $status === Password::RESET_LINK_SENT
+                ? back()->with(['status' => __($status)])
+                : back()->withErrors(['email' => __($status)]);
+})->middleware('guest')->name('password.email');
+
+// Modulo per la reimpostazione della password
+Route::get('/reset-password/{token}', function (string $token) {
+    return view('auth.passwords.reset', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+
+// Gestione invio del modulo reset password
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+ 
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function (User $user, string $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+ 
+            $user->save();
+ 
+            event(new PasswordReset($user));
+        }
+    );
+ 
+    return $status === Password::PASSWORD_RESET
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
+})->middleware('guest')->name('password.update');
+
+/*------------------------
+    BACKOFFICE ROUTES
+------------------------*/
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
